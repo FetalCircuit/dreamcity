75276a8b741276997cf7c2a62cad00e3edfd3d6b
Tiffany Funk <FetalCircuit@users.noreply.github.com>
2017-02-14 10:28:38 -0600

    added files and libraries

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+    createCanvas(600, 600);
+    background(255);
+    stroke(0);
+}
+
+function draw() {
+    //<rect x="0.5" y="-0.5" class="st1" width="599" height="600"/>
+    //	<polygon class="st1" points="
+
+    beginShape();
+    vertex(0.5, 418.5)
+    vertex(192.5, 310.5)
+    vertex(249.5, 411.5)
+    vertex(324.5, 402.5)
+    vertex(274.5, 474.5)
+    vertex(70.5, 484.5)
+    vertex(57.5, 457.5)
+    vertex(-1.5, 450.5)
+    vertex(-2.5, 423.5);
+    endShape();
+
+
+    //  ellipse class="st1" cx="184" cy="119.5" rx="108.5" ry="109"/>
+
+
+}
